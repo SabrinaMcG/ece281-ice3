78abbca6b7b70f7bd1ddec1156d9259971406ffc
SabrinaMcG <110415104+SabrinaMcG@users.noreply.github.com>
2025-02-12 21:03:59 -0700

    ICE simulation push

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -44,12 +44,19 @@ begin
 	
 	   -- Test all zeros input
 	   w_addends <= x"00"; w_Cin <= '0'; wait for 10 ns;
-	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
+	       assert (w_sum = x"0" and w_Cout = '0') report "bad on x00" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad on xFF" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"01"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad on x01" severity failure;
+	   w_addends <= x"F0"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "bad on xF0" severity failure;
+	   w_addends <= x"F9"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"8" and w_Cout = '1') report "bad on xF9" severity failure;
+	   w_addends <= x"E7"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"5" and w_Cout = '1') report "bad on x01" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
